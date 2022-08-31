7903ad3a88a147a70b9adf69503d4b4f213ca4c6
Vuxz123 <ethnicthv123@gmail.com>
2022-08-31 07:24:43 +0700

    -Error_fixed-

--- src/main/java/com/ethinicthv/testfabricmod/item/tool/ToolItemImplement.java
+++ src/main/java/com/ethinicthv/testfabricmod/item/tool/ToolItemImplement.java
@@ -2,18 +2,21 @@ package com.ethinicthv.testfabricmod.item.tool;
 
 import com.ethinicthv.testfabricmod.initializing.Util;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
-import org.spongepowered.asm.mixin.Implements;
 
+@SuppressWarnings("EmptyMethod")
 public interface ToolItemImplement {
-    public VoxelShape[] getHitPoint();
-    public void setHitPoint(VoxelShape[] hit);
-    public default int checkHit(BlockPos pos, PlayerEntity player, double distance){
+    VoxelShape[] getHitPoint();
+    void setHitPoint(VoxelShape[] hit);
+    @SuppressWarnings("AssignmentUsedAsCondition")
+    default int checkHit(BlockPos pos, PlayerEntity player, double distance){
         for(int i = 0; i < this.getHitPoint().length; i++){
             boolean bl;
-            if(bl = (Util.checkHit(this.getHitPoint()[i],player,pos, 2).getType() != HitResult.Type.MISS)){
+            BlockHitResult bhr = Util.checkHit(this.getHitPoint()[i],player,pos, distance);
+            if(bl = (bhr != null && bhr.getType() != HitResult.Type.MISS)){
                 return i;
             }
         }
